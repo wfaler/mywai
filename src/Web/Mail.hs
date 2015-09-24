aceb2178174e25140199b537a008361c34b69b16
Wille Faler <wfaler@Willes-MacBook-Pro.local>
2015-09-24 22:35:00 +0200

    more exports

--- src/Web/Mail.hs
+++ src/Web/Mail.hs
@@ -1,5 +1,5 @@
 {-# LANGUAGE OverloadedStrings,TemplateHaskell #-}
-module Web.Mail(sendEmail)where
+module Web.Mail(MailConfig(..),MailConfigurator(..),sendEmail) where
 
 import Control.Monad.Trans.Reader
 import Control.Monad.IO.Class
@@ -13,8 +13,12 @@ data MailConfig = MailConfig { server :: String, port :: Integer, login :: Strin
 class MailConfigurator a where
   mailConfig :: a -> MailConfig
 
+instance MailConfigurator MailConfig where
+  mailConfig = id
+
 sendEmail :: MailConfigurator a => Mail -> (ReaderT a IO) ThreadId
 sendEmail mail = do
   config <- fmap mailConfig (ask :: (ReaderT a IO) a)
   liftIO $ forkIO $ ML.sendMailWithLogin' (server config) (fromInteger (port config)) (login config) (password config) mail
+
 
