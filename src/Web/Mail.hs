c0fc33e56bed0062ba78061137667dbc4b421cc0
Wille Faler <wfaler@Willes-MacBook-Pro.local>
2015-09-25 18:51:07 +0200

    mkMail

--- src/Web/Mail.hs
+++ src/Web/Mail.hs
@@ -1,8 +1,10 @@
 {-# LANGUAGE OverloadedStrings,TemplateHaskell #-}
-module Web.Mail(MailConfig(..),MailConfigurator(..),sendEmail) where
+module Web.Mail(MailConfig(..),MailConfigurator(..),sendEmail,mail) where
 
 import Control.Monad.Trans.Reader
 import Control.Monad.IO.Class
+import Data.Text
+import qualified Data.Text.Lazy as TL
 
 import qualified Network.Mail.SMTP as ML
 import Network.Mail.Mime
@@ -21,4 +23,15 @@ sendEmail mail = do
   config <- fmap mailConfig (ask :: (ReaderT a IO) a)
   liftIO $ forkIO $ ML.sendMailWithLogin' (server config) (fromInteger (port config)) (login config) (password config) mail
 
+from       = Address (Just "Code Qualified")  "hello@codequalified.com"
+to         = [Address (Just "Wille Faler") "wille.faler@gmail.com"]
+cc         = []
+bcc        = []
+subject    = "email subject"
+body       = ML.plainTextPart "email body"
+html       = ML.htmlPart "<h1>HTML</h1>"
 
+mail = ML.simpleMail from to cc bcc subject [body, html]
+
+mkMail :: Address -> [Address] -> Text -> TL.Text -> Mail
+mkMail from to subject body = ML.simpleMail from to [] [] subject [ML.plainTextPart body, ML.htmlPart body]
